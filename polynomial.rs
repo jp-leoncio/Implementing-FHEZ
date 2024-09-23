fb1720b06d762dd969e1c3baeb1470c28baf184e
João Pedro Leôncio <aljpleoncio@gmail.com>
2024-09-23 01:45:50 -0300

    Added subtration and multiplication

--- polynomial.rs
+++ polynomial.rs
@@ -1,7 +1,7 @@
 use std::cmp::max;
 use std::convert::TryInto;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Copy)]
 struct Monomial {
     coef: i32,
     expo: i32,
@@ -13,13 +13,19 @@ struct Polynomial {
 }
 
 fn print_poly(poly: Polynomial) {
-    let mut count = 1;
+    let mut real_count = 1;
+    let mut count = poly.n;
     for mono in &poly.monomial {
-        print!("{:?}x^{:?}", mono.coef, mono.expo);
-        if count < poly.n {
-            print!(" + "); 
+        if mono.coef == 0 {
+            count -= 1;
         }
-        count += 1;
+        else if mono.coef != 0 {
+            print!("{:?}x^{:?} ", mono.coef, mono.expo);
+            if real_count < count {
+                print!("+ "); 
+            }
+            real_count += 1;
+        }
     }
     println!(" ");
 }
@@ -31,7 +37,6 @@ fn add_poly(a: Polynomial, b: Polynomial) -> Polynomial {
     let mon2 = b.monomial.clone();
     let mut i = 0usize;
     let mut j = 0usize;
-    let mut _gerson = Monomial{coef: 0, expo: 0};
 
     while i < a.n.try_into().unwrap() && j < b.n.try_into().unwrap() {
         if mon1[i].expo > mon2[j].expo {
@@ -64,8 +69,31 @@ fn add_poly(a: Polynomial, b: Polynomial) -> Polynomial {
     return c;
 }
 
+#[allow(dead_code)]
+fn sub_poly(a: Polynomial, mut b: Polynomial) -> Polynomial {
+    for mono in &mut b.monomial {
+        mono.coef = mono.coef * (-1);
+    }
+    return add_poly(a, b);
+}
+
+fn mul_poly(a: Polynomial, b: Polynomial) -> Polynomial {
+    let mut v = vec!();
+    let mut mono = Monomial{coef: 0, expo: 0};
+
+    for a_mono in &a.monomial {
+        for b_mono in &b.monomial {
+            mono.coef = a_mono.coef * b_mono.coef;
+            mono.expo = a_mono.expo + b_mono.expo;
+            v.push(mono.clone());
+        }
+    }
+    let c = Polynomial{n: max(a.n, b.n), monomial: v.clone()};
+    return c;
+}
+
 fn main() {
-    let coef = 5;
+    let coef = -5;
     let expo = 3;
     let a = Monomial{coef, expo};
 
@@ -81,6 +109,7 @@ fn main() {
     print_poly(poly.clone());
     print_poly(poly2.clone());
 
-    poly = add_poly(poly, poly2);
+    //poly = sub_poly(poly, poly2);
+    poly = mul_poly(poly, poly2);
     print_poly(poly.clone());
 }
