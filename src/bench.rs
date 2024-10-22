0b53b8eac33363efa36412ea99a1020abeacfd2c
João Pedro Leôncio <aljpleoncio@gmail.com>
2024-10-22 20:03:46 -0300

    Fixed typos

--- src/bench.rs
+++ src/bench.rs
@@ -7,8 +7,8 @@ fn main() {
 
 fn rand_poly_pair(num_coeficients: usize) -> (Polynomial, Polynomial) {
     let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
-    let a = Polynomial::_new_rand(&mut rng, num_coeficients);
-    let b = Polynomial::_new_rand(&mut rng, num_coeficients);
+    let a = Polynomial::new_rand(&mut rng, num_coeficients);
+    let b = Polynomial::new_rand(&mut rng, num_coeficients);
     return (a, b);
 }
 
@@ -25,7 +25,7 @@ fn add_poly<const SCALE: usize>(bencher: divan::Bencher) {
     bencher
         .with_inputs(|| rand_poly_pair(pow_10(SCALE)))
         .bench_refs(|(a, b)| {
-            polynomial::_add_poly(a, b);
+            polynomial::add_poly(a, b);
         })
 }
 
@@ -34,7 +34,7 @@ fn sub_poly<const SCALE: usize>(bencher: divan::Bencher) {
     bencher
         .with_inputs(|| rand_poly_pair(pow_10(SCALE)))
         .bench_refs(|(a, b)| {
-            polynomial::_sub_poly(a, b);
+            polynomial::sub_poly(a, b);
         })
 }
 
@@ -45,7 +45,7 @@ mod mutiplication {
         bencher
             .with_inputs(|| rand_poly_pair(pow_10(SCALE)))
             .bench_refs(|(a, b)| {
-                polynomial::_mul_poly_fast(a, b);
+                polynomial::mul_poly_fast(a, b);
             })
     }
     #[divan::bench(consts = [0,1,2,3,4,5,6])]
@@ -53,7 +53,7 @@ mod mutiplication {
         bencher
             .with_inputs(|| rand_poly_pair(pow_10(SCALE)))
             .bench_refs(|(a, b)| {
-                polynomial::_par_mul_poly(a, b);
+                polynomial::par_mul_poly(a, b);
             })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 mod polynomial;
 mod polynomial_old;
 mod time_comparision;
+mod eval;
 use polynomial::*;
 use crate::time_comparision::*;
 
